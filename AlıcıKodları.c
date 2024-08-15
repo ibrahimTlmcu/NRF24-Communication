043b3fe2263b1fa96dd03854e82ea136d9b0f559
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2024-08-16 00:08:08 +0300

    Update AlıcıKodları.c

--- AlıcıKodları.c
+++ AlıcıKodları.c
@@ -1,3 +1,6 @@
+
+
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
@@ -7,46 +10,65 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
 // OLED display object
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-//NRF24L01 
+
+//NRF24L01
 RF24 radio(9, 8);  // CE, CSN
-//haberleşme kanalı (herhangi bir "string" olabilir)
+
+//verici ile alıcının kanal isimleri aynı olmalıdır
 const byte address[8] = "robolink";
+
 void setup()
 {
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+  while (!Serial);
+    Serial.begin(9600);
+  
+  radio.begin();
+    // Initialize the OLED display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  display.clearDisplay();
   display.display();
 
-  display.clearDisplay();
-  radio.begin();
+  //iletişim adresi tanımla
+  radio.openReadingPipe(0, address);                                                                                                                                                                                                                                                                                        
   
-  //haberleşme adresini tanımla 
-  radio.openWritingPipe(address);
-  
-  //verici olarak ayarla
-  radio.stopListening();
+  //modülü alıcı olarak ayarla
+  radio.startListening();
 }
 
 void loop()
 {
-  //sürekli mesaj gönder
+  //eğer data geldiyse okumaya başla
+  if (radio.available())
+  {
+    char text[32] = {0}; //en fazla 32 karakter bir veri alabileceğinden dolayı, 32 karakterlik 0 lar kümesi oluşturduk. 
+    //Gelen tüm karakterleri buraya sırayla geçirmiş olacağız 
+    
+    radio.read(&text, sizeof(text));                                                                                                                                          
+    display.clearDisplay();
+    display.setTextSize(2); // 2X scaled text
+    display.setTextColor(WHITE);
+    display.setCursor(10, 0);
+    display.println(text); // Display received text
+    display.display(); // Update the display
+    Serial.println(text); //Seri ekrana alınan veriyi yazdır
+    delay(10);
+  }
+  else{
+delay(10);
+    display.clearDisplay();
+    display.setTextSize(2); // 2X scaled text
+    display.setTextColor(WHITE);
+    display.setCursor(10, 0);
+   
+    display.display(); // Update the display
+    display.println("Basarisiz !! "); //Seri ekrana alınan veriyi yazdır                                                                                                                                    
+    
+  }
   
-  const char text[] = "CONNECTED ! "; //mesaj en fazla 32 karakter olabilir
-  radio.write(&text, sizeof(text)); 
-   // OLED ekranı temizle ve yazdır
-  display.clearDisplay();
-  display.setTextSize(2); // 2X ölçekli metin
-  display.setTextColor(WHITE);
-  display.setCursor(10, 0);
-  display.println("Gonderim Basladi"); // X pozisyonunu ekranda göster
-
-  display.setCursor(10, 30); // Y pozisyonunu ikinci satırda göster
-  delay(1000); //1 saniye bekle
 }
-
-
-
